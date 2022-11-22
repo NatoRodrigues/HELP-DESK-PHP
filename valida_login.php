49b5fa99ebb49c7b96ab0a6c59fc517d50ce90a5
NatoRodrigues <renwise2.0@gmail.com>
2022-11-21 23:30:11 -0300

    ajustes validação

--- valida_login.php
+++ valida_login.php
@@ -3,16 +3,16 @@ $usuarios = array(
     array('email' => 'admin@gmail.com', 'password' => '123456'),
     array('email' => 'admin2@gmail.com', 'password' => '123456789'),
 );
-    $errormsg = true;
+    $error_autentica = true;
 
     foreach ($usuarios as $user) {
        if($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']){
-            $errormsg = false;
+            $erro_autentica = false;
             //VARIAVEL DE ERRO COM VALOR FALSO = USUARIO AUTENTICADO
        }
     }
 
-    if ($errormsg == false) {
+    if ($erro_autentica == false) {
         header('Location: home.php');
     } else {
         header('Location: index.php?login=erro');
