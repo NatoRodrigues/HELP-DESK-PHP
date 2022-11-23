117a37a6e5039f392b2fc231cee000c936d42fde
Renato Rodrigues <99916443+NatoRodrigues@users.noreply.github.com>
2022-11-23 10:03:12 -0300

    atualização na validação
    atualização na validação com uso de session

--- valida_login.php
+++ valida_login.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-$_SESSION['x'] = 'Oi, eu sou um valor de sessão!';
+$_SESSION['x'] = 'sessão iniciada';
 print_r($_SESSION);
 echo '<hr/>';
 print_r($_SESSION['y']);
@@ -15,21 +15,8 @@ $usuarios_app = array(
     array('email' => 'adm@teste.com.br', 'senha' => '123456'),
     array('email' => 'user@teste.com.br', 'senha' => 'abcd')
 );
-/*
-
-echo '<pre>';
-print_r($usuarios_app);
-echo '</pre>';
-
-*/
 
 foreach($usuarios_app as $user){
-    /*
-    echo 'Usuario app: ' . $user['email'] . '/' . $user['senha'];
-    echo '<br />';
-    echo 'Usuario form: ' . $_POST['email'] . '/' . $_POST['senha'];
-    echo '<hr />';
-    */
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autenticado = true;
     }
@@ -43,16 +30,4 @@ foreach($usuarios_app as $user){
         $_SESSION['autenticado'] = 'NÃO';
         header('Location: index.php?login=erro');
       }
-
-
-/*
-print_r($_GET)
-
-echo '<br />'
-echo $_GET['email']
-echo '<br />'
-echo $_GET['senha']
-print_r($_POST);
-*/
-
 ?>
